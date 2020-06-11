d47e0bf51af99f24d3a56bc6b67cc1f2d12e64a2
olivier.henley <olivier.henley@gmail.com>
2020-06-11 16:25:30 -0400

    remove unused import

--- src/covidsim.adb
+++ src/covidsim.adb
@@ -2,7 +2,7 @@ with Qt; use Qt;
 with Qt.QApplication; use Qt.QApplication;
 with Qt.QWidget; use Qt.QWidget;
 with CovidSimForm; use CovidSimForm;
-with COVID_19; use COVID_19;
+
 procedure covidsim is
 
 begin
